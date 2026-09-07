f7953abbd85837bb9fd00097d90079727d65881e
Mamuun <almamun19403@gmail.com>
2026-09-07 17:42:52 +0600

    Object

---
+++ Module 1/object.js
@@ -0,0 +1,15 @@
+let student = [
+    {
+        name: "Abdullah",
+        age: 21,
+        city: "Rajshahi"
+    },
+    {
+        name: "Rahim",
+        age: 20,
+        city: "Dhaka"
+    }
+]
+
+console.log(student[1].age);
+// Output: Rahim
