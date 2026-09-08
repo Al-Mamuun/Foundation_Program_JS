d6036179662b7d30c5e2b9b726fb96a67e2cb6d6
Mamuun <almamun19403@gmail.com>
2026-09-08 17:33:29 +0600

    Mini Project using Basic Of JS

--- Module 1/miniProject.js
+++ Module 1/miniProject.js
@@ -1,55 +1,73 @@
 //mini project
 let students = [
-  { id: 101, name: "Rahim", age: 20, department: "CSE" },
-  { id: 102, name: "Karim", age: 22, department: "EEE" },
+    { id: 101, name: "Rahim", age: 20, department: "CSE" },
+    { id: 102, name: "Karim", age: 22, department: "EEE" },
 ];
 
-function addStudent (name,age,dept){
-    let newID = students.length > 0 
-    ? students[students.length - 1] . id + 1 
-    : 101 ;
+function addStudent(name, age, dept) {
+    let newID = students.length > 0
+        ? students[students.length - 1].id + 1
+        : 101;
 
     let newStudent = {
-        id : newID,
-        name : name,
-        age : age,
-        department : dept 
+        id: newID,
+        name: name,
+        age: age,
+        department: dept
     };
 
     students.push(newStudent);
     console.log("Student Added Succesfully");
 };
 
-addStudent("Mamun",22,"IT");
+addStudent("Mamun", 22, "IT");
 console.log(students);
 
-console.log();
-console.log("All Students: ");
-function getAll(){
-    for(let student of students){
+function getAll() {
+    for (let student of students) {
         console.log(student);
     }
 }
 getAll();
 
-console.log();
-console.log("Find Student: ");
 
-function findStudent(ids){
+function findStudent(ids) {
     let foundStudent = null;
 
-    for ( let student of students){
-        if(student.id == ids){
+    for (let student of students) {
+        if (student.id == ids) {
             foundStudent = student;
             break;
         }
     }
 
-    if(foundStudent){
-        console.log("Student Found: ", foundStudent);
-    }else{
+    if (foundStudent) {
+        console.log("Found Student: ", foundStudent);
+    } else {
         console.log("404 Page Not Found");
     }
 };
 
 findStudent(101);
+
+
+function deleteStudent(id) {
+    let targetStudent = -1;
+
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].id == id) {
+            targetStudent = i;
+            break;
+        }
+    }
+
+    if (targetStudent != -1) {
+            let deletedStd = students.splice(targetStudent, 1);
+            console.log(`deleted ${deletedStd[0].name} id: ${targetStudent} deleted`);
+        } else {
+            console.log("404 not found");
+        }   
+}
+
+deleteStudent(101);
+getAll();
