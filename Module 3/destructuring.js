734eef19780ff00345781faab30c046ce98c70d4
Mamuun <almamun19403@gmail.com>
2026-09-09 14:24:14 +0600

    Module 3 Destructuring.JS

--- Module 3/destructuring.js
+++ Module 3/destructuring.js
@@ -26,3 +26,23 @@ console.log(student1.address.city);
 
 const {name:Mamun,age:Age,address:{city,zip}} = student1;
 console.log(Mamun,Age);
+
+
+const arr = ["Red","Green","Blue"];
+const [first,second,] = arr;
+const [,,third] = arr;
+console.log(first,third);
+
+const student2 = {
+  name: "fahim",
+  age: 20,
+  address: {
+    city: "Dhaka",
+    zip: 1212,
+  },
+  hobbies: ["Gardening", "Painting"],
+};
+
+const {name,age,hobbies:[mamun,sakib]} = student2;
+
+console.log(mamun);
