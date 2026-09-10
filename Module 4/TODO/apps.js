06d01b9cabda473d55785718945f8ad33df79279
Mamuun <almamun19403@gmail.com>
2026-09-11 00:47:20 +0600

    My To Do App

---
+++ Module 4/TODO/apps.js
@@ -0,0 +1,10 @@
+console.log('My To Do App');
+
+let taskInput = document.getElementById('taskInput');
+let addBtn = document.getElementById('addBtn');
+let taskList = document.getElementById('taskList');
+// console.log(taskInput, addBtn, taskList);
+
+// let addBtnAlt = document.querySelector('button');
+// let addBtnAlt = document.querySelector('#addBtn');
+// console.log(addBtnAlt);
