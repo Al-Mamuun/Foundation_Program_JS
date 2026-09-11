9fd0314dcab03a71e88bf865dd90e5a8306553cc
Mamuun <almamun19403@gmail.com>
2026-09-11 12:12:44 +0600

    My To Do App

--- Module 4/TODO/apps.js
+++ Module 4/TODO/apps.js
@@ -1,10 +1,45 @@
-console.log('My To Do App');
+let taskInput = document.getElementById("taskInput");
+let addBtn = document.getElementById("addBtn");
+let taskList = document.getElementById("taskList");
 
-let taskInput = document.getElementById('taskInput');
-let addBtn = document.getElementById('addBtn');
-let taskList = document.getElementById('taskList');
-// console.log(taskInput, addBtn, taskList);
-
-// let addBtnAlt = document.querySelector('button');
-// let addBtnAlt = document.querySelector('#addBtn');
+// let addBtnAlt = document.querySelector("button");
+// let addBtnAlt = document.querySelector("#addBtn");
 // console.log(addBtnAlt);
+
+// addBtn.addEventListener("click",function(){
+//     console.log("Clicked");
+// });
+
+addBtn.addEventListener("click",() => {
+  let li = document.createElement("li");
+
+  let span = document.createElement("span");
+  span.textContent = taskInput.value;
+  //let btnwrapper = document.createElement("div");
+  let btnwrapper = document.createElement("span");
+  
+  let completeBtn = document.createElement("button");
+  completeBtn.textContent = "✅";
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌";
+
+  btnwrapper.appendChild(completeBtn);
+  btnwrapper.appendChild(deleteBtn);
+
+  li.appendChild(span);
+  li.appendChild(btnwrapper);
+
+  taskList.appendChild(li);
+  taskInput.value == " ";
+});
+
+// taskInput.addEventListener("keypress",(event) => {
+//   if (event.key === "Enter"){
+//     console.log("Mamun",taskInput.value);
+//   }
+// });
+
+// let li = document.createElement("li");
+// li.textContent = "Mamun";
+// taskList.appendChild(li);
+
