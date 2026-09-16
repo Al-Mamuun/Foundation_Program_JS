97b3286ca63d0f7377e8899843b6076470d67d5f
Mamuun <almamun19403@gmail.com>
2026-09-16 13:32:26 +0600

    Create React Practise app

--- Module 7/react-practise/src/component/card.jsx
+++ Module 7/react-practise/src/component/card.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./card.css";
 
 //* props and children
 
 export default function Card({name, age}) {
+
+  const [member, setMember] = useState(" ");
+
+  const clickHandler = (memberName) => {
+    console.log("Greetings from " + memberName);
+  }
+  console.log(member);
+
+  // const anotherClickHandler = () => {
+  //   console.log("Greetings from another click handler");
+  // }
+
+  // const greetings = (name) => {
+  //   clickHandler(name);
+  //   anotherClickHandler();
+  // }
   return(
-    <div className="card">
+    <div 
+
+    onClick={() => clickHandler(name)}
+    className="card">
     <p>Name: {name}</p>
     <p>Age: {age}</p>
   </div>
