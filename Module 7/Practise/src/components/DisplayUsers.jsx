694a41ee52db370f79c686f92105f96f2f286d5e
Mamuun <almamun19403@gmail.com>
2026-09-22 13:33:37 +0600

    Error handling and modern syntax

--- Module 7/Practise/src/components/DisplayUsers.jsx
+++ Module 7/Practise/src/components/DisplayUsers.jsx
@@ -4,18 +4,56 @@ import Card from "./Card";
 function DisplayUsers() {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .finally(() => {
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => {
+        
+  //       if (!res.ok) {
+  //         throw new Error(res.message || "Error in fetching data");
+  //       }
+  //       // throw new Error("Error in fetching data");
+  //       return res.json();
+        
+  //     })
+  //     .then((data) => setData(data))
+  //     .catch((err) => setError(err.message))
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  useEffect(() => { 
+
+    const fetchUseres = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users"); 
+    
+        if (!res.ok) {
+          throw new Error(res.message || "Error in fetching data");
+        }
+
+        const data = await res.json();
+        setData(data);
+
+      } catch (err) {
+        setError(err.message);
+        
+      } finally {
         setIsLoading(false);
-      });
+      }
+    };
+
+    fetchUseres();
   }, []);
 
   if (isLoading) {
     return <p>Loading......</p>;
+  }
+
+  if (error) {
+    return <h1>{error}</h1>;
   }
 
   return (
