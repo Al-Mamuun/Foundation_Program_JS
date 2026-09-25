f96a9b1080e027f93ef94817c3f1650311e34683
Mamuun <almamun19403@gmail.com>
2026-09-25 21:22:54 +0600

    Practise

--- PractiseWeather/src/pages/About.jsx
+++ PractiseWeather/src/pages/About.jsx
@@ -3,7 +3,14 @@ function About() {
   return (
     <div className="about">
       <h2>Welcome to Weather App!</h2>
-      <p>Now you can check the weather.</p>
+          <p>Now you can check the weather.</p>
+          <form className='weather-form'>
+              <input type="text" id="city" name="city" placeholder="Enter city name" />
+              <div>OR</div>
+              <input type="text" id="city" name="city" placeholder="Map Location" />
+              <br />
+            <button type="submit">Get Weather</button>
+          </form>
     </div>
   );
 }
